f2088d77087941f9461628971c7c882b437fbd6e
GitHub Action <action@github.com>
2023-03-04 12:55:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/jeremiahengland/intellijpyright/MyBundle.kt
+++ src/main/kotlin/com/github/jeremiahengland/intellijpyright/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jeremiahengland.intellijpyright
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jeremiahengland/intellijpyright/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jeremiahengland/intellijpyright/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jeremiahengland.intellijpyright.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jeremiahengland.intellijpyright.MyBundle
 
 class MyApplicationService {
 
